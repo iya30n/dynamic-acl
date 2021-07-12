acae30c8e91f173d2222e7e4a0af0cdc6bd22aae
iya30n <varmeziyaryasin@gmail.com>
2021-07-12 23:35:49 +0430

    get all defined routes

---
+++ src/Route.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DynamicAcl;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Artisan;
+
+class Route
+{
+	/** 
+	 * Get all routes
+	 * 
+	 * @return Illuminate\Support\Collection
+	 */
+	private function getCollected(): Collection
+	{
+		Artisan::call('route:list --json');
+
+		return collect(
+			json_decode(Artisan::output(), true)
+		);
+	}
+
+	/** 
+	 * Get developer defined routes (filter by controllers namespace)
+	 * 
+	 * @return Illuminate\Support\Collection
+	 */
+	public function getUserDefined(): Collection
+	{
+		return $this->getCollected()->filter(function ($route) {
+			$controllerBasePath = config('dynamicACL.controllers_path');
+
+			if (\Str::contains($route['action'], $controllerBasePath))
+				return $route;
+		});
+	}
+}
