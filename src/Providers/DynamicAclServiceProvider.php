c2ee1a4ba97161df7ca31d3f4a5f7992041c7e50
iya30n <varmeziyaryasin@gmail.com>
2021-07-13 17:35:49 +0430

    load package routes

--- src/Providers/DynamicAclServiceProvider.php
+++ src/Providers/DynamicAclServiceProvider.php
@@ -4,13 +4,15 @@ namespace Iya30n\DynamicAcl\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class DynamicAclServiceProvider extends ServiceProvider
+Class DynamicAclServiceProvider extends ServiceProvider
 {
 	public function boot()
 	{
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
 
         $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'dynamicACL');
+
+        $this->loadRoutesFrom(__DIR__ .'/../../routes/web.php');
 
         $this->publishes([
             __DIR__.'/../../config/dynamicACL.php' => config_path('dynamicACL.php'),
