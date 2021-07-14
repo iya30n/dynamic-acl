6467d8f0a4e68ae32025bbb08d26d4edf21d16f2
iya30n <varmeziyaryasin@gmail.com>
2021-07-14 13:56:06 +0430

    add hasPermission method to User model using macroable package.

--- src/Providers/DynamicAclServiceProvider.php
+++ src/Providers/DynamicAclServiceProvider.php
@@ -3,20 +3,40 @@
 namespace Iya30n\DynamicAcl\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use \Javoscript\MacroableModels\Facades\MacroableModels;
 
-Class DynamicAclServiceProvider extends ServiceProvider
+class DynamicAclServiceProvider extends ServiceProvider
 {
-	public function boot()
-	{
+    public function boot()
+    {
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
 
         $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'dynamicACL');
 
-        $this->loadRoutesFrom(__DIR__ .'/../../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
 
         $this->publishes([
-            __DIR__.'/../../config/dynamicACL.php' => config_path('dynamicACL.php'),
-            __DIR__.'/../../database/migrations' => database_path('migrations')
+            __DIR__ . '/../../config/dynamicACL.php' => config_path('dynamicACL.php'),
+            __DIR__ . '/../../database/migrations' => database_path('migrations')
         ]);
-	}
+
+        $this->registerMacros();
+    }
+
+    private function registerMacros()
+    {
+        MacroableModels::addMacro(config('auth.providers.users.model'), 'hasPermission', function ($access) {
+            if ($access == '' || $access == 'dashboard') return true;
+
+            foreach ($this->roles as $role) {
+                $userPermissions = (strpos($access, '.') != false) ?
+                    array_dot($role->permissions) :
+                    $role->permissions;
+
+                return isset($userPermissions[$access]) || isset($userPermissions['fullAccess']);
+            }
+
+            return false;
+        });
+    }
 }
