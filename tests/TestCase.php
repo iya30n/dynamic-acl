d25b1e9eb5e25a068b8255058091b3cbaf2b0cf2
yasin <yascanary0@gmail.com>
2022-04-07 12:15:48 +0430

    tests can be run now.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,9 @@ abstract class TestCase extends Orchestra\Testbench\TestCase
 {
 	protected function getPackageProviders($app)
     {
-        return ['Iya30n\DynamicAcl\Providers\DynamicAclServiceProvider'];
+        return [
+            'Iya30n\DynamicAcl\Providers\DynamicAclServiceProvider',
+            'Javoscript\MacroableModels\MacroableModelsServiceProvider'
+        ];
     }
 }
