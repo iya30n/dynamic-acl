6e47dfbf17e6446e848d4a0a2c30f81b2d1827f4
yasin <yascanary0@gmail.com>
2022-04-07 19:37:58 +0430

    user model for testing

---
+++ tests/Dependencies/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Dependencies;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Orchestra\Testbench\Concerns\WithFactories;
+
+class User extends Model 
+{
+    use WithFactories;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
