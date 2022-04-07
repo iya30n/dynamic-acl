ad67c71319abe7ea00b316979d214af2dbe3f100
yasin <yascanary0@gmail.com>
2022-04-07 19:35:45 +0430

    set config and routes on TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,12 +1,48 @@
 <?php
 
+use Tests\Dependencies\User;
+
 abstract class TestCase extends Orchestra\Testbench\TestCase
 {
-	protected function getPackageProviders($app)
+    protected function getPackageProviders($app)
     {
         return [
             'Iya30n\DynamicAcl\Providers\DynamicAclServiceProvider',
             'Javoscript\MacroableModels\MacroableModelsServiceProvider'
         ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('dynamicACL', [
+            'controller_path' => 'tests/Dependencies'
+        ]);
+
+        $app['config']->set('auth', [
+            'providers' => [
+                'users' => [
+                    'driver' => 'eloquent',
+                    'model' => User::class
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * Define routes setup.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     *
+     * @return void
+     */
+    protected function defineRoutes($router)
+    {
+        $router->get('admin/posts', function () {
+            return 'post list';
+        })->middleware('DynamicAcl')->name('admin.posts');
+
+        $router->get('admin/posts{post}', function () {
+            return 'single post';
+        })->name('admin.posts.show');
     }
 }
