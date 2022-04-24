6d9b32f1f3d2e1b9633e1353eb718bd38ef0f372
iya30n <varmeziyaryasin@gmail.com>
2022-04-24 13:03:04 +0430

    check if package uses in easy panel, ignore roles routes

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,23 @@
 
 use Iya30n\DynamicAcl\Http\Controllers\RoleController;
 
-Route::group([
-    'middleware' => ['web', 'dynamicAcl'],
-    'prefix' => 'admin',
-    'name' => 'admin.'
-], function($router) {
-    // TODO: name does not work on route group (find a way for it)
 
-//	Route::resource('roles', RoleController::class);
-//    $router->resource('roles', RoleController::class);
-
-    Route::get('roles', [RoleController::class, 'index'])->name('admin.roles.index');
-    Route::get('roles/create', [RoleController::class, 'create'])->name('admin.roles.create');
-    Route::post('roles', [RoleController::class, 'store'])->name('admin.roles.store');
-    Route::get('roles/{role}', [RoleController::class, 'edit'])->name('admin.roles.edit');
-    Route::patch('roles/{role}', [RoleController::class, 'update'])->name('admin.roles.update');
-    Route::get('roles/{role}/delete', [RoleController::class, 'destroy'])->name('admin.roles.delete');
-});
+if (!config()->has('easy_panel')) {
+    Route::group([
+        'middleware' => ['web', 'dynamicAcl'],
+        'prefix' => 'admin',
+        'name' => 'admin.'
+    ], function($router) {
+        // TODO: name does not work on route group (find a way for it)
+    
+    //	Route::resource('roles', RoleController::class);
+    //    $router->resource('roles', RoleController::class);
+    
+        Route::get('roles', [RoleController::class, 'index'])->name('admin.roles.index');
+        Route::get('roles/create', [RoleController::class, 'create'])->name('admin.roles.create');
+        Route::post('roles', [RoleController::class, 'store'])->name('admin.roles.store');
+        Route::get('roles/{role}', [RoleController::class, 'edit'])->name('admin.roles.edit');
+        Route::patch('roles/{role}', [RoleController::class, 'update'])->name('admin.roles.update');
+        Route::get('roles/{role}/delete', [RoleController::class, 'destroy'])->name('admin.roles.delete');
+    });
+}
